f426ad14072153d1eac63a44d8ef6e8f6c36e1e2
RebelliousKiwi04 <69890256+RebelliousKiwi04@users.noreply.github.com>
2021-06-16 17:47:33 +1200

    Possibly working

--- Other Tools/TextEditor/TextReader.py
+++ Other Tools/TextEditor/TextReader.py
@@ -2,7 +2,6 @@ from ctypes import *
 import os
 import clr
 path = os.path.abspath("DatTools.dll")
-datassembler = cdll.LoadLibrary(path)
-clr.AddReference(datassembler.Program)
-
-datassembler.Program.Main('/b')
+clr.AddReference("DatTools.dll")
+from datassembler import Program
+Main('/b')
