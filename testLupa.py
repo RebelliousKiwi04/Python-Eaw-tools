a7a4e310cd0408821061444551b98f87fc9ffb06
RebelliousKiwi04 <69890256+RebelliousKiwi04@users.noreply.github.com>
2021-06-21 10:57:33 +1200

    Some more basic EaW lua funcs

--- testLupa.py
+++ testLupa.py
@@ -1,17 +1,57 @@
 import lupa
 from lupa import LuaRuntime
-lua = LuaRuntime()
+
 
 class GameObject:
-    def __init__(self):
-        pass
+    def __init__(self, obj_name):
+        self.name = obj_name
+    
+class Player:
+    def __init__(self, name):
+        self.name = name
 
-lua.globals().GameObject = GameObject
+class BasicEaWFunctions:
+    def Find_First_Object(obj_name):
+        return GameObject(obj_name)
+    def Find_All_Objects_Of_Type():
+        pass
+    def Find_Player(name):
+        return Player(name)
+    def StringCompare(x,y):
+        if x != y:
+            return False
+        else:
+            return True
+# class GameObjectType:
+#     def __init__(self)
+
+def printToTerminal(val):
+    print(str(val))
+
+def init_eaw_environment(file=None, Object=None):
+    lua = LuaRuntime()
+    lua.globals().Script = file
+    lua.globals().ServiceRate = 1
+    lua.globals().UnitServiceRate = 1
+    lua.globals().LuaThreadTable = []
+    lua.globals().LuaWrapperMetaTable = []
+    lua.globals().PlayerObject = None
+    lua.globals().Target= None
+    lua.globals().AITarget = None
+    lua.globals().Object = Object
+    lua.globals().StringCompare = StringCompare
+    lua.globals().Game_Message = printToTerminal
+
+    lua.globals().Find_First_Object = BasicEaWFunctions.Find_First_Object
+    return lua
 try:
+    file = 'test'
+    lua = init_eaw_environment(file)
+
     lua.execute('''
 
     function testFunc(string)
-        print(GameObject)
+        print(Find_First_Object('hi').name)
     end
 
     testFunc('Hiiii Lua')
